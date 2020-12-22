fe839cf51a8851d211ca537ca987848939a16681
Sebastian G <bastik.tor@googlemail.com>
2020-12-22 16:19:42 +0100

    Update qsbieapi_de.ts
    Initial translation of strings into German

--- SandboxiePlus/QSbieAPI/qsbieapi_de.ts
+++ SandboxiePlus/QSbieAPI/qsbieapi_de.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="Sandboxie/BoxedProcess.cpp" line="78"/>
         <source>Terminated</source>
-        <translation type="unfinished"></translation>
+        <translation>Beendet</translation>
     </message>
     <message>
         <location filename="Sandboxie/BoxedProcess.cpp" line="80"/>
         <source>Suspended</source>
-        <translation type="unfinished"></translation>
+        <translation>Ausgesetzt</translation>
     </message>
     <message>
         <location filename="Sandboxie/BoxedProcess.cpp" line="81"/>
         <source>Running</source>
-        <translation type="unfinished"></translation>
+        <translation>Laufend</translation>
     </message>
 </context>
 <context>
@@ -24,106 +24,106 @@
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="97"/>
         <source>Delete protection is enabled for the sandbox</source>
-        <translation type="unfinished"></translation>
+        <translation>Löschschutz ist für diese Sandbox aktiviert</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="118"/>
         <location filename="Sandboxie/SandBox.cpp" line="304"/>
         <location filename="Sandboxie/SandBox.cpp" line="313"/>
         <source>Waiting for folder: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Warten auf Ordner: %1</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="127"/>
         <source>Deleting folder: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Lösche Ordner: %1</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="131"/>
         <source>Error deleting sandbox folder: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler beim Löschen von Sandbox-Ordner: %1</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="152"/>
         <source>A sandbox must be emptied before it can be renamed.</source>
-        <translation type="unfinished"></translation>
+        <translation>Eine Sandbox muss geleert werden, bevor sie umbenannt werden kann.</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="165"/>
         <source>A sandbox must be emptied before it can be deleted.</source>
-        <translation type="unfinished"></translation>
+        <translation>Eine Sandbox muss geleert werden, bevor Sie gelöscht werden kann.</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="206"/>
         <source>Failed to move directory &apos;%1&apos; to &apos;%2&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Konnte Ordner &apos;%1&apos; nicht nach &apos;%2&apos; verschieben</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="215"/>
         <source>Can&apos;t take a snapshot while processes are running in the box.</source>
-        <translation type="unfinished"></translation>
+        <translation>Es kann kein Schnappschuss (Snapshot) erstellt werden, während Prozesse in der Box laufen.</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="228"/>
         <source>Failed to create directory for new snapshot</source>
-        <translation type="unfinished"></translation>
+        <translation>Konnte den Ordner für den neuen Schnappschuss (Snapshot) nicht erstellen</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="230"/>
         <source>Failed to copy RegHive to snapshot</source>
-        <translation type="unfinished"></translation>
+        <translation>Konnte RegHive nicht in den Schnappschuss (Snapshot) kopieren</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="255"/>
         <location filename="Sandboxie/SandBox.cpp" line="418"/>
         <location filename="Sandboxie/SandBox.cpp" line="442"/>
         <source>Snapshot not found</source>
-        <translation type="unfinished"></translation>
+        <translation>Schnappschuss (Snapshot) nicht gefunden</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="258"/>
         <source>Can&apos;t remove a snapshots while processes are running in the box.</source>
-        <translation type="unfinished"></translation>
+        <translation>Es kann kein Schnappschuss (Snapshot) gelöscht werden, während Prozesse in der Box laufen.</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="274"/>
         <source>Can&apos;t remove a snapshots that is shared by multiple later snapshots</source>
-        <translation type="unfinished"></translation>
+        <translation>Es kann kein Schnappschuss (Snapshot) gelöscht werden der von mehreren späteren Schnappschüssen geteilt wird</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="322"/>
         <source>Merging folders: %1 &gt;&gt; %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Ordner zusammenführen: %1 &gt;&gt; %2</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="326"/>
         <source>Error merging snapshot directories &apos;%1&apos; with &apos;%2&apos;, the snapshot has not been fully merged.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler beim Zusammenführen der Schnappschuss Ordner: &apos;%1&apos; with &apos;%2&apos;, der Schnappschuss wurde nicht vollständig zusammengeführt.</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="339"/>
         <source>Failed to remove old snapshot directory &apos;%1&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Konnte alten Schnappschuss-Ordner &apos;%1&apos; nicht entfernen</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="366"/>
         <source>Finishing Snapshot Merge...</source>
-        <translation type="unfinished"></translation>
+        <translation>Beende Schnappschusszusammenführung...</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="421"/>
         <source>Can&apos;t switch snapshots while processes are running in the box.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wechsel zwischen Schnappschüssen nicht möglich, während Prozesse in der Box laufen.</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="424"/>
         <source>Failed to remove old RegHive</source>
-        <translation type="unfinished"></translation>
+        <translation>Konnte alten RegHive nicht entfernen</translation>
     </message>
     <message>
         <location filename="Sandboxie/SandBox.cpp" line="426"/>
         <source>Failed to copy RegHive from snapshot</source>
-        <translation type="unfinished"></translation>
+        <translation>Konnte RegHive nicht vom Schnappschuss (Snapshot) kopieren</translation>
     </message>
 </context>
 <context>
@@ -131,87 +131,89 @@
     <message>
         <location filename="SbieAPI.cpp" line="272"/>
         <source>Failed to connect to driver</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler beim Verbinden mit dem Treiber</translation>
     </message>
     <message>
         <location filename="SbieAPI.cpp" line="290"/>
         <source>Incompatible Version, found Sandboxie %1, compatible versions: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Inkompatible Version, Sandboxie %1 gefunden, kompatible Versionen: %2</translation>
     </message>
     <message>
         <location filename="SbieAPI.cpp" line="444"/>
         <source>request %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Anfrage %1</translation>
     </message>
     <message>
         <location filename="SbieAPI.cpp" line="448"/>
         <source>early reply</source>
-        <translation type="unfinished"></translation>
+        <translation>Frühe Antwort</translation>
     </message>
     <message>
         <location filename="SbieAPI.cpp" line="455"/>
         <source>mismatched reply</source>
-        <translation type="unfinished"></translation>
+        <translation>Inkongruente Antwort</translation>
     </message>
     <message>
         <location filename="SbieAPI.cpp" line="464"/>
         <source>null reply (msg %1 len %2)</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Kept &apos;msg&apos; and &apos;len&apos; untranslated</translatorcomment>
+        <translation>Keine Antwort (msg %1 len%2)</translation>
     </message>
     <message>
         <location filename="SbieAPI.cpp" line="502"/>
         <source>reply %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Antwort %1</translation>
     </message>
     <message>
         <location filename="SbieAPI.cpp" line="906"/>
         <source>Can&apos;t find Sandboxie instal path.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kann Installationspfad von Sandboxie nicht finden.</translation>
     </message>
     <message>
         <location filename="SbieAPI.cpp" line="977"/>
         <source>You are not authorized to update configuration in section &apos;%1&apos;</source>
-        <translation type="unfinished"></translation>
+        <translation>Sie sind nicht berechtigt die Konfiguration in Sektion &apos;%1&apos; zu aktualisieren</translation>
     </message>
     <message>
         <location filename="SbieAPI.cpp" line="979"/>
         <source>Failed to set configuration setting %1 in section %2: %3</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler beimSetzen der Konfigurationsoption %1 in Sektion %2: %3</translation>
     </message>
     <message>
         <location filename="SbieAPI.cpp" line="1045"/>
         <source>The sandbox name can not be longer than 32 charakters.</source>
-        <translation type="unfinished"></translation>
+        <translation>Der Name der Sandbox darf nicht länger als 32 Zeichen sein.</translation>
     </message>
     <message>
         <location filename="SbieAPI.cpp" line="1052"/>
         <source>The sandbox name can not be a device name.</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Kept original &apos;device name&apos; in brackets to ensure it can be looked up online much easier</translatorcomment>
+        <translation>Der Name der Sandbox darf kein Gerätename (device name) sein.</translation>
     </message>
     <message>
         <location filename="SbieAPI.cpp" line="1055"/>
         <source>The sandbox name can contain only letters, digits and underscores which are displayed as spaces.</source>
-        <translation type="unfinished"></translation>
+        <translation>Der Name der Sandbox darf nur Buchstaben, Zahlen und Unterstriche, welche als Leerstellen angezeigt werden, enthalten.</translation>
     </message>
     <message>
         <location filename="SbieAPI.cpp" line="1282"/>
         <source>Failed to terminate all processes</source>
-        <translation type="unfinished"></translation>
+        <translation>Konnte nicht alle Prozesse beenden</translation>
     </message>
     <message>
         <location filename="Sandboxie/SbieIni.cpp" line="230"/>
         <source>Failed to copy configuration from sandbox %1: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler beim Kopieren der Konfiguartion von Sandbox %1: %2</translation>
     </message>
     <message>
         <location filename="Sandboxie/SbieIni.cpp" line="237"/>
         <source>A sandbox of the name %1 already exists</source>
-        <translation type="unfinished"></translation>
+        <translation>Es existiert bereits eine Sandbox mit dem Namen %1</translation>
     </message>
     <message>
         <location filename="Sandboxie/SbieIni.cpp" line="260"/>
         <source>Failed to delete sandbox %1: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler beim Löschen der Sandbox %1: %2</translation>
     </message>
 </context>
 <context>
@@ -219,12 +221,12 @@
     <message>
         <location filename="SbieStatus.h" line="22"/>
         <source>Error Code: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler Code: %1</translation>
     </message>
     <message>
         <location filename="SbieUtils.cpp" line="313"/>
         <source>Open %1 in sandbox %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Öffne %1 in Sandbox %2</translation>
     </message>
 </context>
 </TS>

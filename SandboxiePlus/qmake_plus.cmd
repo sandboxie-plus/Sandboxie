6a6e634c0011e7b9c8d990366bcf4f9c401be8f9
DavidXanatos <xanatosdavid@gmail.com>
2021-01-31 00:06:03 +0100

    Update qmake_plus.cmd

--- SandboxiePlus/qmake_plus.cmd
+++ SandboxiePlus/qmake_plus.cmd
@@ -3,49 +3,48 @@ REM echo Current dir: %cd%
 REM echo folder: %~dp0
 REM echo arch: %1
 
-set my_dir=%~dp0
 
 IF %1 == Win32 (
   call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
-  set qt_path=%my_dir%..\..\Qt\5.15.1\msvc2019
+  set qt_path=%~dp0..\..\Qt\5.15.1\msvc2019
   set build_arch=Win32
 )
 IF %1 == x64 (
   call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-  set qt_path=%my_dir%..\..\Qt\5.15.1\msvc2019_64
+  set qt_path=%~dp0..\..\Qt\5.15.1\msvc2019_64
   set build_arch=x64
 )
 
 
-mkdir %my_dir%\Build_qtsingleapp_%build_arch%
-cd %my_dir%\Build_qtsingleapp_%build_arch%
+mkdir %~dp0\Build_qtsingleapp_%build_arch%
+cd %~dp0\Build_qtsingleapp_%build_arch%
 
-%qt_path%\bin\qmake.exe %my_dir%\QtSingleApp\qtsingleapp\qtsingleapp\qtsingleapp.qc.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
-%my_dir%..\..\Qt\Tools\QtCreator\bin\jom.exe -f Makefile.Release -j 8
-
-
-mkdir %my_dir%\Build_MiscHelpers_%build_arch%
-cd %my_dir%\Build_MiscHelpers_%build_arch%
-
-%qt_path%\bin\qmake.exe %my_dir%\MiscHelpers\MiscHelpers.qc.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
-%my_dir%..\..\Qt\Tools\QtCreator\bin\jom.exe -f Makefile.Release -j 8
+%qt_path%\bin\qmake.exe %~dp0\QtSingleApp\qtsingleapp\qtsingleapp\qtsingleapp.qc.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
+%~dp0..\..\Qt\Tools\QtCreator\bin\jom.exe -f Makefile.Release -j 8
 
 
-mkdir %my_dir%\Build_QSbieAPI_%build_arch%
-cd %my_dir%\Build_QSbieAPI_%build_arch%
+mkdir %~dp0\Build_MiscHelpers_%build_arch%
+cd %~dp0\Build_MiscHelpers_%build_arch%
 
-%qt_path%\bin\qmake.exe %my_dir%\QSbieAPI\QSbieAPI.qc.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
-%my_dir%..\..\Qt\Tools\QtCreator\bin\jom.exe -f Makefile.Release -j 8
-
-
-mkdir %my_dir%\Build_SandMan_%build_arch%
-cd %my_dir%\Build_SandMan_%build_arch%
-
-%qt_path%\bin\qmake.exe %my_dir%\SandMan\SandMan.qc.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
-%my_dir%..\..\Qt\Tools\QtCreator\bin\jom.exe -f Makefile.Release -j 8
+%qt_path%\bin\qmake.exe %~dp0\MiscHelpers\MiscHelpers.qc.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
+%~dp0..\..\Qt\Tools\QtCreator\bin\jom.exe -f Makefile.Release -j 8
 
 
-cd %my_dir%
+mkdir %~dp0\Build_QSbieAPI_%build_arch%
+cd %~dp0\Build_QSbieAPI_%build_arch%
+
+%qt_path%\bin\qmake.exe %~dp0\QSbieAPI\QSbieAPI.qc.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
+%~dp0..\..\Qt\Tools\QtCreator\bin\jom.exe -f Makefile.Release -j 8
+
+
+mkdir %~dp0\Build_SandMan_%build_arch%
+cd %~dp0\Build_SandMan_%build_arch%
+
+%qt_path%\bin\qmake.exe %~dp0\SandMan\SandMan.qc.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
+%~dp0..\..\Qt\Tools\QtCreator\bin\jom.exe -f Makefile.Release -j 8
+
+
+cd %~dp0
 dir .\bin
 dir .\bin\%build_arch%
 dir .\bin\%build_arch%\Release

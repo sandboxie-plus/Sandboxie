c6f3030eaabe75463597aa8aed98b2a0c0c5f4cf
DavidXanatos <xanatosdavid@gmail.com>
2021-01-31 00:04:42 +0100

    Update install_jom.cmd

--- SandboxiePlus/install_jom.cmd
+++ SandboxiePlus/install_jom.cmd
@@ -1,2 +1,3 @@
-curl --ssl-no-revoke -L https://download.qt.io/online/qtsdkrepository/windows_x86/desktop/tools_qtcreator/qt.tools.qtcreator/4.14.0-0-202012170949jom.7z -o %my_dir%../../jom.7z
-"C:\Program Files\7-Zip\7z.exe" x -o%my_dir%..\..\Qt\ %my_dir%../../jom.7z
+curl --ssl-no-revoke -L https://download.qt.io/online/qtsdkrepository/windows_x86/desktop/tools_qtcreator/qt.tools.qtcreator/4.14.0-0-202012170949jom.7z -o %~dp0../../jom.7z
+"C:\Program Files\7-Zip\7z.exe" x -o%~dp0..\..\Qt\ %~dp0../../jom.7z
+

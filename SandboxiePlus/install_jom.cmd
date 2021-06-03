8797d58d49c2816a660c6675aa0e7e2b158c287f
DavidXanatos <xanatosdavid@gmail.com>
2021-06-03 17:16:58 +0200

    fix jom

--- SandboxiePlus/install_jom.cmd
+++ SandboxiePlus/install_jom.cmd
@@ -1,4 +1,4 @@
-curl --ssl-no-revoke -L https://download.qt.io/online/qtsdkrepository/windows_x86/desktop/tools_qtcreator/qt.tools.qtcreator/4.14.0-0-202012170949jom.7z -o %~dp0..\..\jom.7z
+curl --ssl-no-revoke -L https://download.qt.io/online/qtsdkrepository/windows_x86/desktop/tools_qtcreator/qt.tools.qtcreator/4.15.0-0-202105040231jom.7z -o %~dp0..\..\jom.7z
 "C:\Program Files\7-Zip\7z.exe" x -o%~dp0..\..\Qt\ %~dp0..\..\jom.7z
 
 dir %~dp0..\..\

0a87aec1fd9fcdca4258d5cd2b2f452fd59a200a
DavidXanatos <xanatosdavid@gmail.com>
2021-10-24 12:32:38 +0200

    Update my_version.h

--- Sandboxie/common/my_version.h
+++ Sandboxie/common/my_version.h
@@ -21,8 +21,8 @@
 #ifndef _MY_VERSION_H
 #define _MY_VERSION_H
 
-#define MY_VERSION_BINARY       5,53,1
-#define MY_VERSION_STRING       "5.53.1"
+#define MY_VERSION_BINARY       5,53,2
+#define MY_VERSION_STRING       "5.53.2"
 #define MY_VERSION_COMPAT		"5.53.0" // this refers to the driver ABI compatibility
 
 // These #defines are used by either Resource Compiler, or by NSIC installer

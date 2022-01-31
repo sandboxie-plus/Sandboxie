0598770caae5a5bf1212eb48213b5f8195fd7c45
isaak654 <isaak654@users.noreply.github.com>
2022-01-31 17:06:50 +0100

    Fix NSIS commented line

--- Sandboxie/common/my_version.h
+++ Sandboxie/common/my_version.h
@@ -25,7 +25,7 @@
 #define MY_VERSION_STRING       "5.55.10"
 #define MY_VERSION_COMPAT		"5.55.0" // this refers to the driver ABI compatibility
 
-// These #defines are used by either Resource Compiler, or by NSIC installer
+// These #defines are used by either Resource Compiler or NSIS installer
 #define SBIE_INSTALLER_PATH		"..\\Bin\\"
 #define SBIE_INSTALLER_PATH_32  "..\\Bin\\Win32\\SandboxieInstall32.exe"
 #define SBIE_INSTALLER_PATH_64  "..\\Bin\\x64\\SandboxieInstall64.exe"

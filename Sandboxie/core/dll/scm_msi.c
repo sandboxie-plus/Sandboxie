aed569ec98db0c6711a54c4336fccc989df847e1
DavidXanatos <xanatosdavid@gmail.com>
2022-03-26 10:14:51 +0100

    Update scm_msi.c

--- Sandboxie/core/dll/scm_msi.c
+++ Sandboxie/core/dll/scm_msi.c
@@ -16,8 +16,6 @@
  *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "handle.h"
-
 //---------------------------------------------------------------------------
 // Service Control Manager
 //---------------------------------------------------------------------------

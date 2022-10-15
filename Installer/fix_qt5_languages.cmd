38f075a30631eb402f782f231ad3c710eef84d90
isaak654 <isaak654@users.noreply.github.com>
2022-10-15 12:02:29 +0200

    Remove get_qttranslations.cmd
    It had already been renamed as fix_qt5_languages.cmd

--- Installer/fix_qt5_languages.cmd
+++ Installer/fix_qt5_languages.cmd
@@ -1,8 +1,8 @@
 mkdir %~dp0qttranslations
 mkdir %~dp0qttranslations\ts
 mkdir %~dp0qttranslations\qm
-set fileName=qttranslations-everywhere-src-6.2.3.zip
-set downloadUrl=https://download.qt.io/archive/qt/6.2/6.2.3/submodules/%filename%
+set fileName=qttranslations-everywhere-src-6.3.1.zip
+set downloadUrl=https://download.qt.io/archive/qt/6.3/6.3.1/submodules/%filename%
 curl -LsSO --output-dir %~dp0qttranslations\ %downloadUrl%
 "C:\Program Files\7-Zip\7z.exe" e -i!*\translations\qt_*.ts -i!*\translations\qtbase_*.ts -i!*\translations\qtmultimedia_*.ts %~dp0qttranslations\%filename% -bd -o%~dp0qttranslations\ts\
 for %%a in (%~dp0qttranslations\ts\*.ts) do (lrelease.exe -silent %%a -qm %~dp0qttranslations\qm\%%~na.qm)

74a103e1de59c6790f85b9d24318c438d04e50b7
DavidXanatos <xanatosdavid@gmail.com>
2023-04-24 18:49:55 +0200

    Update get_assets.cmd

--- Installer/get_assets.cmd
+++ Installer/get_assets.cmd
@@ -32,41 +32,36 @@ copy %~dp0..\sandboxie\install\SandboxieVS.nsi %~dp0\Assets\Classic\install\
 copy %~dp0..\sandboxie\install\Warning.ini %~dp0\Assets\Classic\install\
 mkdir %~dp0\Assets\Classic\msgs
 mkdir %~dp0\Assets\Classic\msgs\SbieRelease
-
-dir %~dp0..\sandboxie\
-dir %~dp0..\sandboxie\msgs\
-dir %~dp0..\sandboxie\msgs\SbieRelease\
-
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Albanian.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Arabic.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Bulgarian.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Croatian.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Czech.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Danish.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Dutch.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_English.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Estonian.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Farsi.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Finnish.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_French.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_German.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Greek.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Hebrew.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Hungarian.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Indonesian.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Italian.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Japanese.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Korean.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Macedonian.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Polish.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Portuguese.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_PortugueseBr.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Russian.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_SimpChinese.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Slovak.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Spanish.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Swedish.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_TradChinese.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Turkish.txt %~dp0..\sandboxie\msgs\SbieRelease\
-copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Ukrainian.txt %~dp0..\sandboxie\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Albanian.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Arabic.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Bulgarian.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Croatian.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Czech.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Danish.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Dutch.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_English.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Estonian.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Farsi.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Finnish.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_French.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_German.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Greek.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Hebrew.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Hungarian.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Indonesian.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Italian.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Japanese.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Korean.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Macedonian.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Polish.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Portuguese.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_PortugueseBr.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Russian.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_SimpChinese.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Slovak.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Spanish.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Swedish.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_TradChinese.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Turkish.txt %~dp0\Assets\Classic\msgs\SbieRelease\
+copy %~dp0..\sandboxie\msgs\SbieRelease\NsisText_Ukrainian.txt %~dp0\Assets\Classic\msgs\SbieRelease\
 

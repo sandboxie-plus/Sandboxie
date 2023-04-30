5540c23e6d45607f42f2dd470a81a97790d062f1
Sebastian G <bastik.tor@googlemail.com>
2023-04-30 10:30:34 +0200

    Update NewBoxWizard.cpp
    another typo fixed, thanks to offhub

--- SandboxiePlus/SandMan/Wizards/NewBoxWizard.cpp
+++ SandboxiePlus/SandMan/Wizards/NewBoxWizard.cpp
@@ -478,7 +478,7 @@ CAdvancedPage::CAdvancedPage(QWidget *parent)
     registerField("boxToken", pBoxToken);
 
     QCheckBox* pImageProtection = new QCheckBox(tr("Prevent sandboxes programs installed on host from loading dll's from the sandbox"));
-    pImageProtection->setToolTip(tr("This feature may reduce compatibility as it also prevents box located processes from writing to host located once and even starting them."));
+    pImageProtection->setToolTip(tr("This feature may reduce compatibility as it also prevents box located processes from writing to host located ones and even starting them."));
     pImageProtection->setChecked(theConf->GetBool("BoxDefaults/ImagesProtection", false));
     pImageProtection->setEnabled(g_CertInfo.valid);
     layout->addWidget(pImageProtection, row++, 1, 1, 3);

ae0b5c3f6504c8254918b434823a8a2618dea5dd
isaak654 <isaak654@users.noreply.github.com>
2023-05-08 22:10:46 +0200

    Update SandManRecovery.cpp
    \r\n is not supported in Qt: 111dfcf8d34649b8051d88ad0d7abeaa52b2b064

--- SandboxiePlus/SandMan/SandManRecovery.cpp
+++ SandboxiePlus/SandMan/SandManRecovery.cpp
@@ -175,7 +175,7 @@ void CSandMan::RecoverFilesAsync(QPair<const CSbieProgressPtr&,QWidget*> pParam,
 						int retVal = 0;
 						QMetaObject::invokeMethod(theGUI, "ShowQuestion", Qt::BlockingQueuedConnection, // show this question using the GUI thread
 							Q_RETURN_ARG(int, retVal),
-							Q_ARG(QString, tr("The file %1 failed a security check, do you want to recover it anyway?\r\n\r\n%2").arg(BoxPath).arg(Output)),
+							Q_ARG(QString, tr("The file %1 failed a security check, do you want to recover it anyway?\n\n%2").arg(BoxPath).arg(Output)),
 							Q_ARG(QString, tr("Do this for all files!")),
 							Q_ARG(bool*, &forAll),
 							Q_ARG(int, QDialogButtonBox::Yes | QDialogButtonBox::No),

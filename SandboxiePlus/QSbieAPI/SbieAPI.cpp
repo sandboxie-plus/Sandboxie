e9886c1bcaa4a8ec9a88cf4e7ec8ee3bc076b0d1
DavidXanatos <3890945+DavidXanatos@users.noreply.github.com>
2023-05-15 20:08:09 +0200

    Update SbieAPI.cpp

--- SandboxiePlus/QSbieAPI/SbieAPI.cpp
+++ SandboxiePlus/QSbieAPI/SbieAPI.cpp
@@ -832,14 +832,6 @@ void CSbieAPI::OnReloadConfig()
 		ReloadConfig();
 }
 
-typedef struct _FILE_FS_VOLUME_INFORMATION {
-  LARGE_INTEGER VolumeCreationTime;
-  ULONG         VolumeSerialNumber;
-  ULONG         VolumeLabelLength;
-  BOOLEAN       SupportsObjects;
-  WCHAR         VolumeLabel[1];
-} FILE_FS_VOLUME_INFORMATION, *PFILE_FS_VOLUME_INFORMATION;
-
 extern "C" {
 	NTSTATUS NTAPI RtlSetThreadErrorMode(IN ULONG NewMode, OUT PULONG OldMode);
 }

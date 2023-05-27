9455e96a699cbc665f791e191f2a13bb40783ab0
DavidXanatos <3890945+DavidXanatos@users.noreply.github.com>
2023-05-27 19:18:16 +0200

    1.9.6

--- Sandboxie/core/dll/ole.cpp
+++ Sandboxie/core/dll/ole.cpp
@@ -130,7 +130,7 @@ extern "C" _FX BOOLEAN Ole_Init(HMODULE module)
         RegisterDragDrop = GetProcAddress(module, "RegisterDragDrop");
         RevokeDragDrop   = GetProcAddress(module, "RevokeDragDrop");
 
-        if (Gui_RenameClasses) {
+        if (Gui_RenameClasses && Config_GetSettingsForImageName_bool(L"UseDragDropHack", TRUE)) {
 
             //
             // don't hook drag and drop if using OpenWinClass=*

7a224822de479bb398f6fd66534ac7697b9550cb
nkh0472 <nkh0472@hotmail.com>
2023-07-06 20:17:20 +0800

    A few typo fixes

--- SandboxiePlus/SandMan/sandman_en.ts
+++ SandboxiePlus/SandMan/sandman_en.ts
@@ -5150,7 +5150,7 @@ Processes in this box will be running with a custom process token indicating the
     <message>
         <location filename="Windows/SupportDialog.cpp" line="43"/>
         <source>This Insider build requires a special certificate of type GREAT_PATREON, PERSONAL-HUGE, or CONTRIBUTOR.
-If you are a great patreaon supporter already, sandboxie can check online for an update of your certificate.</source>
+If you are a Great Supporter on Patreon already, Sandboxie can check online for an update of your certificate.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>

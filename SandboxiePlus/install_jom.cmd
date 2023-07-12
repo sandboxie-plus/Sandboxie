03a3f8381d276d3d9dc26c12c20aa127ab0a423a
DavidXanatos <xanatosdavid@gmail.com>
2023-07-12 09:41:53 +0200

    Update install_jom.cmd
    [skip ci]

--- SandboxiePlus/install_jom.cmd
+++ SandboxiePlus/install_jom.cmd
@@ -1,5 +1,9 @@
+if exist %~dp0..\..\Qt\Tools\QtCreator\bin\jom.exe goto done
+
 curl -LsSO --output-dir %~dp0..\..\ https://download.qt.io/official_releases/jom/jom.zip
 "C:\Program Files\7-Zip\7z.exe" x -aoa -o%~dp0..\..\Qt\Tools\QtCreator\bin\ %~dp0..\..\jom.zip
+
+:done
 
 REM dir %~dp0..\..\
 REM dir %~dp0..\..\Qt

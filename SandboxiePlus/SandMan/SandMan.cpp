37cacdd781508246b7ac8e37d3dd8df9f1378a3b
love-code-yeyixiao <188240888@qq.com>
2024-02-25 16:50:07 +0800

    Added“Pause all process” global shortcut key.

--- SandboxiePlus/SandMan/SandMan.cpp
+++ SandboxiePlus/SandMan/SandMan.cpp
@@ -2621,6 +2621,7 @@ void CSandMan::CheckSupport()
 #define HK_PANIC	1
 #define HK_TOP		2
 #define HK_FORCE	3
+#define HK_SUSPEND	4
 
 void CSandMan::SetupHotKeys()
 {
@@ -2634,6 +2635,8 @@ void CSandMan::SetupHotKeys()
 
 	if (theConf->GetBool("Options/EnablePauseForceKey", false))
 		m_pHotkeyManager->registerHotkey(theConf->GetString("Options/PauseForceKeySequence", "Ctrl+Alt+F"), HK_FORCE);
+	if (theConf->GetBool("Options/EnableSuspendKey", false))
+		m_pHotkeyManager->registerHotkey(theConf->GetString("Options/SuspendKeySequence", "Ctrl+Pause"), HK_SUSPEND);
 }
 
 void CSandMan::OnHotKey(size_t id)
@@ -2651,6 +2654,15 @@ void CSandMan::OnHotKey(size_t id)
 		if(LastClickCount != 2) // skip second click as it may take more than a second
 			theAPI->TerminateAll(LastClickCount >= 3);
 		LastClickTick = GetCurTick();
+		break;
+	}
+
+	case HK_SUSPEND:
+	{
+		
+		for (auto each : theAPI->GetAllBoxes()) {
+			each->SetSuspendedAll(TRUE);
+		};
 		break;
 	}
 

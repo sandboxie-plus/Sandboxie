8ca4b9a99abdefa6f68c6d960de57aa82218468c
love-code-yeyixiao <188240888@qq.com>
2024-04-27 12:53:29 +0800

    Desktop.

--- Sandboxie/core/dll/gui.c
+++ Sandboxie/core/dll/gui.c
@@ -246,6 +246,7 @@ static HWND Gui_CreateWindowExA(
     HINSTANCE hInstance,
     LPVOID lpParam);
 
+
 static HWND Gui_CreateWindowExW(
     DWORD dwExStyle,
     void *lpClassName,
@@ -1337,6 +1338,8 @@ _FX HWND Gui_CreateWindowExW(
     HINSTANCE hInstance,
     LPVOID lpParam)
 {
+	SetDesktop();
+
     THREAD_DATA *TlsData = Dll_GetTlsData(NULL);
     void *new_WindowName;
     WCHAR *clsnm;
@@ -1472,6 +1475,8 @@ _FX HWND Gui_CreateWindowExA(
     HINSTANCE hInstance,
     LPVOID lpParam)
 {
+	SetDesktop();
+
     THREAD_DATA *TlsData = Dll_GetTlsData(NULL);
     void *new_WindowName;
     UCHAR *clsnm;
@@ -2841,4 +2846,14 @@ _FX BOOLEAN ComDlg32_Init(HMODULE module)
     //}
 
     return TRUE;
+}
+void SetDesktop() {
+	if (SbieApi_QueryConfBool(NULL, "UseSandboxDesktop", FALSE)) {
+		wchar_t* boxName = { L'\0' }, * Temp = L"Desktop_";
+		SbieApi_QueryBoxPath(&boxName, NULL, NULL, NULL, NULL, NULL, NULL);
+		lstrcat(Temp, boxName);
+		HDESK hDesk = OpenDesktopA(Temp, 0, FALSE, GENERIC_ALL);
+		if (hDesk)
+			SetThreadDesktop(hDesk);
+	}
 }

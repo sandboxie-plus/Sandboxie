67c71aa768fe9158f11ec0d3864e196051dbd29e
love-code-yeyixiao <188240888@qq.com>
2024-07-07 10:44:41 +0800

    Update Start.cpp

--- Sandboxie/apps/start/start.cpp
+++ Sandboxie/apps/start/start.cpp
@@ -31,6 +31,7 @@
 #include "core/drv/api_defs.h"
 #include <psapi.h>
 #include <Shlwapi.h>
+#include<wincred.h>
 
 
 //---------------------------------------------------------------------------
@@ -1232,12 +1233,66 @@ int Program_Start(void)
 
                 ULONG64 ProcessFlags = SbieApi_QueryProcessInfo(0, 0);
                 if (! (ProcessFlags & SBIE_FLAG_DROP_RIGHTS)) {
+					if (SbieApi_QueryConfBool(NULL, L"ElevateWithPwOnly", FALSE)) {
+						PCREDUI_INFOW info = new CREDUI_INFOW();
+						info->cbSize = sizeof(CREDUI_INFO);
+						info->hbmBanner = NULL;
+						info->hwndParent = NULL;
+						info->pszCaptionText = SbieDll_FormatMessage0(3257);
+						info->pszMessageText = SbieDll_FormatMessage0(3258);
+						typedef DWORD (*P_CredUIPromptForCredentialsW)(
+							 PCREDUI_INFOW pUiInfo,
+							           PCWSTR        pszTargetName,
+							           PCtxtHandle   pContext,
+							 DWORD         dwAuthError,
+							      PWSTR         pszUserName,
+							           ULONG         ulUserNameBufferSize,
+							      PWSTR         pszPassword,
+							           ULONG         ulPasswordBufferSize,
+							      BOOL* save,
+							           DWORD         dwFlags
+						);
+						typedef DWORD (*P_CredUIParseUserNameW)(
+							  PCWSTR UserName,
+							 WCHAR* user,
+							  ULONG  userBufferSize,
+							 WCHAR* domain,
+							  ULONG  domainBufferSize
+						);
+						P_CredUIPromptForCredentialsW rupfc = (P_CredUIPromptForCredentialsW)GetProcAddress(GetModuleHandleW(L"Credui.dll"), "CredUIPromptForCredentialsW");
+						P_CredUIParseUserNameW cupun=(P_CredUIParseUserNameW)GetProcAddress(GetModuleHandleW(L"Credui.dll"), "CredUIParseUserNameW");
+						if (rupfc&&cupun)
+						{
+							BOOL bSave = false;
+							LPWSTR username = (WCHAR*)malloc(100), password = (WCHAR*)malloc(100), appName = const_cast<WCHAR*>(L"Start.exe");
+							memset(username, 0, 100);
+							memset(password, 0, 100);
+							if (rupfc(info, appName, NULL, 0, username, 100, password, 100, &bSave, CREDUI_FLAGS_COMPLETE_USERNAME | CREDUI_FLAGS_EXPECT_CONFIRMATION | CREDUI_FLAGS_REQUEST_ADMINISTRATOR | CREDUI_FLAGS_USERNAME_TARGET_CREDENTIALS | CREDUI_FLAGS_VALIDATE_USERNAME | CREDUI_FLAGS_DO_NOT_PERSIST | CREDUI_FLAGS_INCORRECT_PASSWORD | CREDUI_FLAGS_PASSWORD_ONLY_OK) == NO_ERROR) {
+								LPWSTR user = (WCHAR*)malloc(100), domain = (WCHAR*)malloc(100);
+								memset(user, 0, 100);
+								memset(domain, 0, 100);
+								cupun(username, user, 100, domain, 100);
+								HANDLE tempToken = NULL;
+								if (LogonUser(user, domain, password, LOGON32_LOGON_BATCH, LOGON32_PROVIDER_DEFAULT, &tempToken)) {
+									MessageBox(NULL, SbieDll_FormatMessage0(3259), L"Sandboxie Start", MB_OK);
+									goto skipElevate;
+								}
 
+								CloseHandle(tempToken);
+								free(user);
+								free(domain);
+							}
+						}
+						else {
+							MessageBox(NULL, SbieDll_FormatMessage0(3259), L"Sandboxie Start", MB_OK);
+							goto skipElevate;
+						}
+					}
                     shExecInfo.lpVerb = L"runas";
                 }
             }
         }
-
+skipElevate:
         //
         // make sure AppHelp.dll is loaded, so third party software like
         // EMET which relies on injection through ShimEng/AppHelp can work.

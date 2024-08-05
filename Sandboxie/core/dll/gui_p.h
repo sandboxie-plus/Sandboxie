648fd472850314a119e97bbcafd3a639fff01801
love-code-yeyixiao <188240888@qq.com>
2024-08-05 17:12:49 +0800

    fix

--- Sandboxie/core/dll/gui_p.h
+++ Sandboxie/core/dll/gui_p.h
@@ -521,10 +521,10 @@ extern WCHAR* Gui_BoxNameTitleW;
 extern ATOM Gui_DropTargetProp_Atom;
 extern ATOM Gui_WindowProcOldW_Atom;
 extern ATOM Gui_WindowProcOldA_Atom;
-#ifndef PreviousActivedWindow
-#define PreviousActivedWindow
-extern HWND Gui_PreviousActivedWindow = NULL;
-#endif // !PreviousActivedWindow
+#ifndef PreviousActiveWindow
+#define PreviousActiveWindow
+extern HWND Gui_PreviousActiveWindow = NULL;
+#endif // !PreviousActiveWindow
 
 //---------------------------------------------------------------------------
 // Undo Unicode defines from windows.h

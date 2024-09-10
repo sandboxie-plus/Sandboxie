7e7ffff48cc293d5c43b34804796c048cc31b493
VenusGirl❤ <venusgirl@outlook.com>
2024-09-10 12:45:08 +0900

    Update sandman_ko.ts

--- SandboxiePlus/SandMan/sandman_ko.ts
+++ SandboxiePlus/SandMan/sandman_ko.ts
@@ -5471,7 +5471,7 @@ This file is part of Sandboxie and all changed done to it will be reverted next 
         <location filename="Views/SbieView.cpp" line="1040"/>
         <location filename="Views/SbieView.cpp" line="1408"/>
         <source>7-zip Archive (*.7z);;Zip Archive (*.zip)</source>
-        <translation type="unfinished"></translation>
+        <translation>7-zip 압축파일 (*.7z);;Zip 압축파일 (*.zip)</translation>
     </message>
     <message>
         <location filename="Views/SbieView.cpp" line="1057"/>
@@ -7398,7 +7398,7 @@ If you are a great patreaon supporter already, sandboxie can check online for an
         <location filename="Forms/CompressDialog.ui" line="65"/>
         <source>Export Sandbox to a 7z or Zip archive, Choose Your Compression Rate and Customize Additional Compression Settings.</source>
         <oldsource>Export Sandbox to a 7z archive, Choose Your Compression Rate and Customize Additional Compression Settings.</oldsource>
-        <translation type="unfinished">샌드박스를 7z 압축파닐로 내보내고 압축률을 선택하고 추가 압축 설정을 사용자 정의합니다.</translation>
+        <translation>샌드박스를 7z 압축파일로 내보내고 압축률을 선택하고 추가 압축 설정을 사용자 지정합니다.</translation>
     </message>
 </context>
 <context>

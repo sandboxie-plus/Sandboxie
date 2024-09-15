0750445028c8337b3c2b765de1233f7c24844e25
DavidXanatos <3890945+DavidXanatos@users.noreply.github.com>
2024-09-15 13:31:35 +0200

    Update OptionsGeneral.cpp

--- SandboxiePlus/SandMan/Windows/OptionsGeneral.cpp
+++ SandboxiePlus/SandMan/Windows/OptionsGeneral.cpp
@@ -335,10 +335,8 @@ void COptionsWindow::LoadGeneral()
 	ui.chkForceProtection->setChecked(m_pBox->GetBool("ForceProtectionOnMount", false));
 	ui.chkUserOperation->setChecked(m_pBox->GetBool("BlockInterferenceControl", false));
 	ui.chkCoverBar->setChecked(m_pBox->GetBool("AllowCoverTaskbar", false));
-	if (ui.chkRamBox->isEnabled()) {
+	if (ui.chkRamBox->isEnabled())
 		ui.chkEncrypt->setEnabled(!ui.chkRamBox->isChecked());
-		ui.chkForceProtection->setEnabled(!ui.chkRamBox->isChecked());
-	}
 	ui.chkForceProtection->setEnabled(ui.chkEncrypt->isEnabled() && ui.chkEncrypt->isChecked());
 
 	CSandBoxPlus* pBoxEx = qobject_cast<CSandBoxPlus*>(m_pBox.data());

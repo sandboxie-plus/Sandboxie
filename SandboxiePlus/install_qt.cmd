30ab5052e89b56bed957666076ba97b12a4a89a7
offhub <6871698+offhub@users.noreply.github.com>
2024-09-22 19:36:48 +0300

    Qt-5.15.15-OpenSSL-3.3.2 update

--- SandboxiePlus/install_qt.cmd
+++ SandboxiePlus/install_qt.cmd
@@ -1,8 +1,8 @@
 echo %*
-IF "%~7" == "" ( set "ghQtBuilds_hash_x64=ebe1f2351b7313b8b0678aadf81c9edacf04af84572b72a576972ee2fb8d5aea" ) ELSE ( set "ghQtBuilds_hash_x64=%~7" )
-IF "%~6" == "" ( set "ghQtBuilds_hash_x86=13928d0ff1bc461ef1d054aa19f1427b172277c6659d0ccf1999e6ffa850126e" ) ELSE ( set "ghQtBuilds_hash_x86=%~6" )
-IF "%~5" == "" ( set "ghQtBuilds_repo=qt-sbie-builds" ) ELSE ( set "ghQtBuilds_repo=%~5" )
-IF "%~4" == "" ( set "ghQtBuilds_user=offhub" ) ELSE ( set "ghQtBuilds_user=%~4" )
+IF "%~7" == "" ( set "ghQtBuilds_hash_x64=bae6773292ad187aad946854766344c4bd24245359404636b3a1b13d9ae6a97e" ) ELSE ( set "ghQtBuilds_hash_x64=%~7" )
+IF "%~6" == "" ( set "ghQtBuilds_hash_x86=0dc0048be815eeaa76bcdd2d02e7028d21cc75fd7f4fb65445d3adf37b4a75bb" ) ELSE ( set "ghQtBuilds_hash_x86=%~6" )
+IF "%~5" == "" ( set "ghQtBuilds_repo=qt-builds" ) ELSE ( set "ghQtBuilds_repo=%~5" )
+IF "%~4" == "" ( set "ghQtBuilds_user=xanasoft" ) ELSE ( set "ghQtBuilds_user=%~4" )
 IF "%~3" == "" ( set "qt6_version=6.3.1" ) ELSE ( set "qt6_version=%~3" )
 IF "%~2" == "" ( set "qt_version=5.15.15" ) ELSE ( set "qt_version=%~2" )
 

78cfa4c61372dfca085fce712d5bc8a35c572c0b
DavidXanatos <xanatosdavid@gmail.com>
2024-10-19 14:13:46 +0200

    Update copy_build.cmd
    [skipci]

--- Installer/copy_build.cmd
+++ Installer/copy_build.cmd
@@ -26,6 +26,9 @@ IF %1 == ARM64 (
   set qtPath=%~dp0..\..\Qt\%qt6_version%\msvc2019_arm64
   set instPath=%~dp0\SbiePlus_a64
 )
+
+set
+
 set redistPath=%VCToolsRedistDir%\%1\Microsoft.VC142.CRT
 @echo on
 

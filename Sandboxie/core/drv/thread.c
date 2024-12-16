9a086d14833256b0a76c01c988d2558d0cc63167
DavidXanatos <3890945+DavidXanatos@users.noreply.github.com>
2024-12-16 09:51:18 +0100

    Update thread.c
    #4421

--- Sandboxie/core/drv/thread.c
+++ Sandboxie/core/drv/thread.c
@@ -1104,9 +1104,9 @@ _FX ACCESS_MASK Thread_CheckObject_CommonEx(
     // If the calling process is sandboxed the later common check will do the blocking
     //
 
-    if (!proc || proc->bHostInject) { // caller is not sandboxed
+    if (!proc || (proc == PROCESS_TERMINATED) || proc->bHostInject) { // caller is not sandboxed
 
-        if (Process_Find(pid, NULL)) {  // target is sandboxed - lock free check
+        if (pid != NULL && Process_Find(pid, NULL)) {  // target is sandboxed - lock free check
         
             void* nbuf = 0;
             ULONG nlen = 0;

1a0afbe503c7292da3d34e2bf12dbfb29a36a1e5
DavidXanatos <3890945+DavidXanatos@users.noreply.github.com>
2025-03-25 07:38:00 +0100

    Update SbieView.h

--- SandboxiePlus/SandMan/Views/SbieView.h
+++ SandboxiePlus/SandMan/Views/SbieView.h
@@ -200,6 +200,7 @@ private:
 	QAction*				m_pMenuRemove;
 	QMenu*					m_pMenuTools;
 	QAction*				m_pMenuDuplicate;
+	QAction*				m_pMenuDuplicateEx;
 	QAction*				m_pMenuExport;
 	QAction*				m_pMenuMoveUp;
 	//QAction*				m_pMenuMoveBy;

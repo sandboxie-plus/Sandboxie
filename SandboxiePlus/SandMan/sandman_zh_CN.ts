af930ae6f576bcb9adc0ca939ce25ce70f73d5e8
风之暇想 <53783792+fzxx@users.noreply.github.com>
2025-04-14 11:18:23 +0800

    Update sandman_zh_CN.ts

--- SandboxiePlus/SandMan/sandman_zh_CN.ts
+++ SandboxiePlus/SandMan/sandman_zh_CN.ts
@@ -5537,7 +5537,7 @@ Error: %1</source>
     <message>
         <location filename="Views/SbieView.cpp" line="1192"/>
         <source>&lt;br /&gt;</source>
-        <translation></translation>
+        <translation>&lt;br /&gt;</translation>
     </message>
     <message>
         <location filename="Views/SbieView.cpp" line="1194"/>
@@ -5997,7 +5997,7 @@ This is a temporary Patreon certificate, valid for 3 months. Once it nears expir
     <message>
         <location filename="Windows/SettingsWindow.cpp" line="2135"/>
         <source>Auto update in future</source>
-        <translation type="unfinished"></translation>
+        <translation>将来自动更新</translation>
     </message>
     <message>
         <source>You can request a free %1-day evaluation certificate up to %2 times for any one Hardware ID</source>

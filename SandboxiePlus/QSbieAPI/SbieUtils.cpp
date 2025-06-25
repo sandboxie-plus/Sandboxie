e753fb4c52706a7906b5465f4eb3087d0ff99260
offhub <6871698+offhub@users.noreply.github.com>
2025-06-25 23:58:09 +0300

    Update SbieUtils.cpp
    fixes #4882

--- SandboxiePlus/QSbieAPI/SbieUtils.cpp
+++ SandboxiePlus/QSbieAPI/SbieUtils.cpp
@@ -644,14 +644,14 @@ bool CSbieUtils::GetStartMenuShortcut(CSbieAPI* pApi, QString &BoxName, QString 
 	
 
 	struct SLnk {
-		WCHAR box_name[BOXNAME_COUNT];	//0
-		WCHAR reserved[30];				//34
-		WCHAR link_path[956];			//64
-		ULONG IconIndex;				//1020
-		WCHAR unused[2];				//1022
-		WCHAR icon_path[1024];			//1024
-		WCHAR work_dir[1024];			//2048
-										//3072
+		WCHAR box_name[BOXNAME_COUNT];		//0
+		WCHAR reserved[64 - BOXNAME_COUNT];	//40
+		WCHAR link_path[956];				//64
+		ULONG IconIndex;					//1020
+		WCHAR unused[2];					//1022
+		WCHAR icon_path[1024];				//1024
+		WCHAR work_dir[1024];				//2048
+											//3072
 	} *lnk = (SLnk*)buf;
 
 	BoxName = QString::fromWCharArray(lnk->box_name, wcsnlen_s(lnk->box_name, sizeof(lnk->box_name)));

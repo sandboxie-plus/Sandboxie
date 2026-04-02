7f86d2d1364a9de780efc3c6f51f29515d43e87b
Jimbb <14995924+jimbb@users.noreply.github.com>
2026-04-02 12:44:55 +0800

    fix: update 7-zip source and privacy templates

--- Installer/get_7zip.cmd
+++ Installer/get_7zip.cmd
@@ -1,4 +1,27 @@
-mkdir %~dp0\7-Zip
-curl -L --url https://github.com/DavidXanatos/7z/releases/download/23.01/7z2301.zip -o %~dp0\7-Zip\7z2301.zip --ssl-no-revoke
-"C:\Program Files\7-Zip\7z.exe" x -bd -o%~dp0\7-Zip\ %~dp0\7-Zip\7z2301.zip
+@echo off
+setlocal
+
+set "RELEASE_VERSION=26.00"
+set "PACKAGE_VERSION=2600"
+set "SEVENZIP_DIR=%~dp0\7-Zip"
+set "SEVENZIP_WIN32=%SEVENZIP_DIR%\7z%PACKAGE_VERSION%.exe"
+set "SEVENZIP_X64=%SEVENZIP_DIR%\7z%PACKAGE_VERSION%-x64.exe"
+
+mkdir "%SEVENZIP_DIR%" 2>nul
+mkdir "%SEVENZIP_DIR%\7-Zip-Win32" 2>nul
+mkdir "%SEVENZIP_DIR%\7-Zip-x64" 2>nul
+
+curl -fL --url https://github.com/ip7z/7zip/releases/download/%RELEASE_VERSION%/7z%PACKAGE_VERSION%.exe -o "%SEVENZIP_WIN32%" --ssl-no-revoke
+if errorlevel 1 exit /b %errorlevel%
+
+curl -fL --url https://github.com/ip7z/7zip/releases/download/%RELEASE_VERSION%/7z%PACKAGE_VERSION%-x64.exe -o "%SEVENZIP_X64%" --ssl-no-revoke
+if errorlevel 1 exit /b %errorlevel%
+
+"C:\Program Files\7-Zip\7z.exe" x -y -aoa -bd -o"%SEVENZIP_DIR%\7-Zip-Win32" "%SEVENZIP_WIN32%"
+if errorlevel 1 exit /b %errorlevel%
+
+"C:\Program Files\7-Zip\7z.exe" x -y -aoa -bd -o"%SEVENZIP_DIR%\7-Zip-x64" "%SEVENZIP_X64%"
+if errorlevel 1 exit /b %errorlevel%
+
+endlocal
 
